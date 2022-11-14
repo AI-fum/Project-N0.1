eb1fec2322cb5346afb078be4990dbe0a55fc03e
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2022-11-14 21:33:31 +0330

    Update dfs.py

--- Phase 2/DFS/dfs.py
+++ Phase 2/DFS/dfs.py
@@ -37,7 +37,7 @@ class table:
             return False
         return True
 
-    def finish(row, col, tr, tc):
+    def finish(self, row, col, tr, tc):
         if(row == tr and col == tc):
             return True
         else:
@@ -67,5 +67,59 @@ class table:
                 adjx = row + dRow[i]
                 adjy = col + dCol[i]
                 st.append([adjx, adjy])
-            #if(self.finish(row, col, tr, tc)):
-                #break
+            if(self.finish(row, col, tr, tc)):
+                break
+                
+                
+    def printpath(self, row, col, tr, tc):
+        saver = 0
+        savec = 0
+        self.setdf()
+        self.dataframe = self.arrangedf()
+        #print(self.dataframe)
+        self.dataframe = self.strtoint()
+        dRow = [0, 1, 0, -1]
+        dCol = [-1, 0, 1, 0]
+        st = []
+        st.append([row, col])
+        while (len(st) > 0):
+            curr = st[len(st) - 1]
+            st.remove(st[len(st) - 1])
+            row = curr[0]
+            col = curr[1]
+            if (self.isValid(row, col, self.dataframe) == False):
+                #print(curr)
+                #print(row, end = ",")
+                #print(col, end = " ")
+                continue
+
+
+            self.vis[row][col] = True
+            #print(row-saver, end = ",")
+            #print(col-savec, end = " ")
+            
+            if(row-saver == 0):
+                if(col-savec == 0):
+                    print("stay", end =", ")
+                if(col-savec == 1):
+                    print("down", end =", ")
+                if(col-savec == -1):
+                    print("up", end =", ")
+            if(col-savec == 0):
+                if(row-saver == 0):
+                    print("stay", end =", ")
+                if(row-saver == 1):
+                    print("right", end =", ")
+                if(row-saver == -1):
+                    print("left", end =", ")
+                    
+            saver = row
+            savec = col
+            for i in range(4):
+                adjx = row + dRow[i]
+                adjy = col + dCol[i]
+                st.append([adjx, adjy])
+            if(self.finish(row, col, tr, tc)):
+                break
+                
+            
