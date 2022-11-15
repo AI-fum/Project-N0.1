ba8b792b99ef5b3530030e917d33df0b2c90f88a
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2022-11-15 17:22:41 +0330

    Update code.py

--- Phase 1/code.py
+++ Phase 1/code.py
@@ -15,6 +15,14 @@ class table:
     def printdf(self):
         self.setdf()
         return self.dataframe
+    
+    
+    
+    def robotindex(self):
+        for i in range(self.Rows):
+            for j in range(self.Columns):
+                if "r" in self.Matrix[i][j]:
+                    return ([i, j])
         
         
 # An example code to take matrix input by user  
