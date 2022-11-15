4d32ca6ae096b18012611d131343c03b34500295
samaneh <samanehcosmonaut@gmail.com>
2022-11-15 15:14:08 +0330

    Added functions to find index of butter, robot and target

--- Phase 1/code.py
+++ Phase 1/code.py
@@ -7,33 +7,34 @@ class table:
         self.Rows = Rows
         self.Columns = Columns
         self.Matrix = Matrix
-
+        
     def setdf(self):
         self.dataframe = pd.DataFrame(self.Matrix)
-        # return df
-
+        #return df
+        
     def printdf(self):
         self.setdf()
         return self.dataframe
+        
+        
+# An example code to take matrix input by user  
+Rows = int(input("Give the number of rows:"))  
+Columns = int(input("Give the number of columns:"))  
+# Initializing the matrix  
+matrix = []  
+  
+# taking RowsxColumns matrix from the user  
+for i in range(Rows):  
+    # taking input for the row from the user  
+    single_row = list(map(str, input().split()))  
+    # appending the 'single_row' to the 'example_matrix'  
+    matrix.append(single_row)  
+# printing the matrix given by user  
+#print(matrix)  
 
-
-# An example code to take matrix input by user
-Rows = int(input("Give the number of rows:"))
-Columns = int(input("Give the number of columns:"))
-# Initializing the matrix
-matrix = []
-
-# taking RowsxColumns matrix from the user
-for i in range(Rows):
-    # taking input for the row from the user
-    single_row = list(map(str, input().split()))
-    # appending the 'single_row' to the 'example_matrix'
-    matrix.append(single_row)
-# printing the matrix given by user
-# print(matrix)
 
 t1 = table(Rows, Columns, matrix, "")
-# t1.setdf()
+#t1.setdf()
 t1.printdf()
 
 # a function which returns index(es) of butter(s)
