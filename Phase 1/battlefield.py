f30e6f1e148106925b5e0d9f9cda4793a4298d24
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2022-11-28 16:14:50 +0330

    Update battlefield.py

--- Phase 1/battlefield.py
+++ Phase 1/battlefield.py
@@ -9,16 +9,18 @@ class Battlefield:
         self.height = height
         self.points = []
 
-    def check_out_of_bounds(self, y: int, x: int) -> bool:
+    #The selected states should be within the battlefield area...
+    def check_out(self, y: int, x: int) -> bool:
         return x >= self.width or x < 0 or y >= self.height or y < 0
-
+    
+    #This function detects obstacles in the battlefield:
     def is_block(self, y: int, x: int):
         return self.battlefield.[y][x].lower() == 'x'
 
     def set_points(self, points):
         self.points = points
 
-    def get_item(self, y, x) -> str:
+    def get_state(self, y, x) -> str:
         return self.battlefield.[y][x]
 
     def append_row(self, row: list[str]) -> None:
