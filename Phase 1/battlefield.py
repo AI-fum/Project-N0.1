c97189d80a312c4d88aef60e24dfcb8c05addf2a
samaneh <samanehcosmonaut@gmail.com>
2022-11-29 20:40:58 +0330

    Updated battelfield.py

--- Phase 1/battlefield.py
+++ Phase 1/battlefield.py
@@ -1,7 +1,8 @@
 class Battlefield:
-    #map_array
+    # map_array
     matrix: list[list[str]]
     points: list[tuple[int, int]]
+
     def __init__(self, height: int, width: int, matrix=[]):
         self.map = matrix
         self.width = width
@@ -12,15 +13,15 @@ class Battlefield:
         return x >= self.width or x < 0 or y >= self.height or y < 0
 
     def is_block(self, y: int, x: int):
-        return self.battlefield.[y][x].lower() == 'x'
+        return self.battlefield[y][x].lower() == "x"
 
     def set_points(self, points):
         self.points = points
 
     def get_item(self, y, x) -> str:
-        return self.battlefield.[y][x]
+        return self.battlefield[y][x]
 
     def append_row(self, row: list[str]) -> None:
         if len(row) != self.width:
-            raise ValueError('Invalid size of columns in this row:\n', str(row))
+            raise ValueError("Invalid size of columns in this row:\n", str(row))
         self.battlefield.append(row)
