ab77f8477af396cdac9481f551e066e8f46579f4
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2022-12-01 20:03:04 +0330

    Create defaults.py

---
+++ Release/defaults.py
@@ -0,0 +1,20 @@
+class Defaults:
+
+    # Battlefield
+    MAP_FILE = './battlefields/battlefield.txt'
+
+    # Window
+    SCREEN_WIDTH = 1200
+    SCREEN_HEIGHT = 600
+    BACKGROUND = 122, 142, 224
+    SCREEN_MARGIN_SIZE = 40
+    CELL_COLOR = 93, 0, 225
+    BLOCK_COLOR = 32, 32, 32
+    BUTTER_IMAGE = './images/butter.png'
+    ROBOT_IMAGE = './images/robot.png'
+    X_IMAGE = './images/x.png'
+    MARK_IMAGE = './images/mark.png'
+
+    # Times
+    STEP_TIME = 0.2
+    FPS = 2
