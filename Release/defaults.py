a04d5de2c386bd5bd9a4f356a80d45c9069309a9
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2022-12-02 09:26:16 +0330

    Update defaults.py

--- Release/defaults.py
+++ Release/defaults.py
@@ -4,17 +4,22 @@ class Defaults:
     MAP_FILE = './battlefields/battlefield.txt'
 
     # Window
+    # Size:
     SCREEN_WIDTH = 1200
     SCREEN_HEIGHT = 600
     BACKGROUND = 122, 142, 224
     SCREEN_MARGIN_SIZE = 40
+    
+    # Colors:
     CELL_COLOR = 93, 0, 225
     BLOCK_COLOR = 32, 32, 32
+    
+    # Images:
     BUTTER_IMAGE = './images/butter.png'
     ROBOT_IMAGE = './images/robot.png'
     X_IMAGE = './images/x.png'
     MARK_IMAGE = './images/mark.png'
 
-    # Times
+    # Times:
     STEP_TIME = 0.2
     FPS = 2
