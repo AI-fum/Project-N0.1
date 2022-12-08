edf9d2ff6669d3cd26137966a0c46cc82eb4cc66
samaneh <samanehcosmonaut@gmail.com>
2022-12-08 09:42:59 +0330

    Updated phase 2

--- Phase 2/BFS/bfs.py
+++ Phase 2/BFS/bfs.py
@@ -3,20 +3,23 @@ from successor import State
 from node import Node
 
 battlefield: Battlefield
-init_state: State  #initial state...
+init_state: State  # initial state...
 
-def bfs() -> Node:
-  queue = [Node(init_state)]
-  vis = {}  #visited nodes
 
-  while len(queue) > 0:  # Starting BFS loop
-    current_node = queue.pop(0)
-    vis[current_node.state] = current_node
+def bfs(self) -> Node:
+    queue = [Node(init_state)]
+    vis = {}  # visited nodes
+    
+    while len(queue) > 0:  # Starting BFS loop
+        current_node = queue.pop(0)
+        vis[current_node.state] = current_node
 
-    if State.is_goal(current_node.state, self.map.points):
-      return current_node
+        if State.is_goal(current_node.state, self.map.points):
+            return current_node
 
-    actions = State.successor(current_node.state, battlefield)  # Add successors to queue
-      for child in current_node.expand(actions):
-        if child.state not in vis:
-          queue.append(child)
+        actions = State.successor(
+            current_node.state, battlefield
+        )  # Add successors to queue
+        for child in current_node.expand(actions):
+            if child.state not in vis:
+                queue.append(child)
