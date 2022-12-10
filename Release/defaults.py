ee5db6c06d316bd00e8e06cd3d802688bea02ae2
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2022-12-10 10:55:29 +0330

    Update defaults.py

--- Release/defaults.py
+++ Release/defaults.py
@@ -5,17 +5,16 @@ class Defaults:
 
     #IDS
     FIRST_K = 1
-    LAST_K = 100
-    
+    LAST_K = 50
     # Window
     # Size:
     SCREEN_WIDTH = 1200
     SCREEN_HEIGHT = 600
-    BACKGROUND = 122, 142, 224
-    SCREEN_MARGIN_SIZE = 40
+    BACKGROUND = 100, 12, 62
+    SCREEN_MARGIN_SIZE = 10
     
     # Colors:
-    CELL_COLOR = 93, 0, 225
+    CELL_COLOR = 63, 12, 62
     BLOCK_COLOR = 32, 32, 32
     
     # Images:
@@ -23,7 +22,14 @@ class Defaults:
     ROBOT_IMAGE = './images/robot.png'
     X_IMAGE = './images/x.png'
     MARK_IMAGE = './images/mark.png'
-
+    
+    ONE_IMAGE = './images/1.png'
+    TWO_IMAGE = './images/2.png'
+    TREE_IMAGE = './images/3.png'
+    FOUR_IMAGE = './images/4.png'
+    FIVE_IMAGE = './images/5.png'
+    BARRIER_IMAGE = './images/barrier.png'
+    
     # Times:
     STEP_TIME = 0.2
     FPS = 2
