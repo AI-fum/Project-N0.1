32df5b58ae831653945a2019645771fb1fb50f68
salehi_m <salehi_m@office.zharfpouyan.net>
2022-12-10 10:45:54 +0330

    latest update IDS

--- Phase 2/IDS/ids.py
+++ Phase 2/IDS/ids.py
@@ -1,41 +1,51 @@
+from battlefield import Battlefield
+from successor import State
+from node import Node
 
-def iterative_deepening_search(matrix, start, goal, max_depth):
-    for depth_limit in range(max_depth):
-        visited = set()
-        stack = [(start, 0)]
-        while stack:
-            (current, depth) = stack.pop()
-            if current in goal:
-                return True
-            if depth < depth_limit:
-                for neighbor in get_neighbors(matrix, current):
-                    if neighbor not in visited:
-                        visited.add(neighbor)
-                        stack.append((neighbor, depth+1))
-    return False
+battlefield: Battlefield
+init_state: State  # initial state...
+max_depth = 1000
 
+def ids(self) -> Node:
+    depth = 0  # Initialize the depth to 0
 
-def get_neighbors(matrix, current):
-    # Return the neighbors of the current cell in the matrix.
-    (i, j) = current
-    neighbors = []
-    if i > 0:
-        neighbors.append((i-1, j))  # Top neighbor
-    if i < len(matrix)-1:
-        neighbors.append((i+1, j))  # Bottom neighbor
-    if j > 0:
-        neighbors.append((i, j-1))  # Left neighbor
-    if j < len(matrix[0])-1:
-        neighbors.append((i, j+1))  # Right neighbor
-    return neighbors
+    # Loop until the maximum depth is reached
+    while depth <= max_depth:
+        # Perform a depth-first search at the current depth
+        result = self.dfs(depth)
 
-if __name__ == "__main__" :
-    start = robot_state # initial state on robot ... 
-    goals =  battlefield_state # list of state of battlefield
-    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    found = iterative_deepening_search(matrix, start, goals, max_depth=10)
-    if found:
-        print("Goal found!")
-    else:
-        print("Goal not found.")
+        # Check if a solution was found
+        if result is not None:
+            # Return the solution node
+            return result
 
+        # Increment the depth
+        depth += 1
+
+    # If the search failed, return None
+    return None
+
+def dfs(self, depth) -> Node:
+    stack = [Node(init_state)]  # Initialize the stack with the root node
+    vis = {}  # Dictionary to keep track of visited nodes
+
+    # Loop until the stack is empty
+    while len(stack) > 0:
+        # Pop the next node from the stack
+        current_node = stack.pop()
+        vis[current_node.state] = current_node
+        # Check if the node is the goal
+        if State.is_goal(current_node.state, self.battlefield.points):
+            # Return the goal node
+            return current_node
+        # Check if the current depth has been reached
+        if current_node.depth == depth:
+            continue
+        # Generate the successor nodes for the current node
+        actions = State.successor(current_node.state, battlefield)
+        for child in current_node.expand(actions):
+            # Add the child to the stack if it has not been visited
+            if child.state not in vis:
+                stack.append(child)
+    # If the search failed, return None
+    return None
